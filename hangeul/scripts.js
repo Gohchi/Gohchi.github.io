1d4a0979092e95a7950dd4badccaa935357be804
Martín Farias <martin.akg1991@gmail.com>
2020-07-04 15:27:05 -0300

    Adds the menu for instructions and notes. Adds the syllable pronunciation.

--- hangeul/scripts.js
+++ hangeul/scripts.js
@@ -29,9 +29,70 @@ const numbersB = {
   '100': '백',
   '1000': '천',
 };
-function getNumberType(){
-  return get('type').value == "0" ? 'A' : 'B';
+
+let voices;
+setTimeout(() => {
+  loadVoices();
+}, 100);
+function loadVoices(){
+  console.log('Loading voices...');
+  voices = speechSynthesis.getVoices().filter(o => o.lang === 'ko-KR');
+  if(!voices.length) {
+    console.log('Loading voices... fail. Retry');
+    setTimeout(() => {
+      loadVoices();
+    }, 100);
+  }
 }
+
+// tools --------------------------------------------------------------------------------
+
+function get(id){
+  if(isMobile()) id = 'mobile-' + id;
+  return document.getElementById(id);
+}
+
+function set(id, value){
+  document.getElementById(id).innerText = value;
+}
+
+function isEmpty(id){
+  return !document.getElementById(id).innerText;
+}
+
+const groups = {
+  "0": { from: 1000, to: 9999 },
+  "1": { from: 300, to: 999 },
+  "2": { from: 100, to: 299 },
+  "3": { from: 1, to: 99 },
+}
+
+function randNum(){
+  if(getNumberType() == 'A')
+    return Math.floor(Math.random() * 12) + 1;
+
+  const p = Math.random();
+  const i = p < 0.1 ? 0 : p < 0.3 ? 1 : p < 0.6 ? 2 : 3;
+  const group = groups[i];
+  return Math.floor(Math.random() * (group.to - group.from)) + group.from;
+}
+
+function say( text ){
+  let msg = new SpeechSynthesisUtterance( text );
+  msg.lang = 'ko-KR';
+  msg.voice = voices[get('voice').value];
+  msg.rate = parseFloat(get('rate').value);
+  window.speechSynthesis.speak( msg );
+}
+
+function isMobile(){
+  return window.innerWidth <= 900;
+}
+
+function getNumberType(){
+  return get('number-type').value == "0" ? 'A' : 'B';
+}
+
 function numberToText(num){
   if(getNumberType() == 'A')
     return numbersA[num];
@@ -53,20 +114,12 @@ function numberToText(num){
   }
   return name.join('');
 }
-function getRandomNumber(){
-  set('number', randNum());
-  set('hangeul', '');
-}
-function sayMyName(){
+
+
+function getNumberAsHangeul(){
   let num = parseInt(document.getElementById('number').innerText);
 
-  say(numberToText(num));
-}
-function showDetails(){
-  let num = parseInt(document.getElementById('number').innerText);
-
-  let hangeul = numberToText(num);
-  set('hangeul', hangeul || '');
+  return numberToText(num);
 }
 
 function keyDownTextField(e) {
@@ -81,61 +134,62 @@ function keyDownTextField(e) {
   }
 }
 
-let voices;
-setTimeout(() => {
-  loadVoices();
-}, 100);
-function loadVoices(){
-  console.log('Loading voices...');
-  voices = speechSynthesis.getVoices().filter(o => o.lang === 'ko-KR');
-  if(!voices.length) {
-    console.log('Loading voices... fail. Retry');
-    setTimeout(() => {
-      loadVoices();
-    }, 100);
+function hide( el ){
+  el.classList.add("hidden");
+}
+
+function show( el ){
+  if(typeof el === 'string'){
+    el = get(el);
+  }
+  el.classList.remove("hidden");
+}
+
+function toggle( el ){
+  if(typeof el === 'string'){
+    el = get(el);
+  }
+  
+  if(el.classList.contains("hidden")){
+    el.classList.remove("hidden");
+  } else {
+    el.classList.add("hidden");
   }
 }
-
-function say( text ){
-  let msg = new SpeechSynthesisUtterance( text );
-  msg.lang = 'ko-KR';
-  msg.voice = voices[get('voice').value];
-  msg.rate = parseFloat(get('rate').value);
-  window.speechSynthesis.speak( msg );
-}
-
-function get(id){
-  if(isMobile()) id = 'mobile-' + id;
-  return document.getElementById(id);
-}
-function set(id, value){
-  document.getElementById(id).innerText = value;
-}
-function isEmpty(id){
-  return !document.getElementById(id).innerText;
-}
-
-const groups = {
-  "0": { from: 1000, to: 9999 },
-  "1": { from: 300, to: 999 },
-  "2": { from: 100, to: 299 },
-  "3": { from: 1, to: 99 },
-}
-function randNum(){
-  if(getNumberType() == 'A')
-    return Math.floor(Math.random() * 12) + 1;
-
-  const p = Math.random();
-  const i = p < 0.1 ? 0 : p < 0.3 ? 1 : p < 0.6 ? 2 : 3;
-  const group = groups[i];
-  return Math.floor(Math.random() * (group.to - group.from)) + group.from;
-}
+// bottom actions --------------------------------------------------------------------------------
 
 function showMore(){
   const menu = get('more-options');
   menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 }
 
-function isMobile(){
-  return window.innerWidth <= 900;
+function sayMyName(){
+  let num = parseInt(document.getElementById('number').innerText);
+
+  say(numberToText(num));
+}
+
+function showDetails(){
+  let hangeul = getNumberAsHangeul();
+  set('hangeul', hangeul || '');
+}
+
+function getRandomNumber(){
+  set('number', randNum());
+  set('hangeul', '');
+  get('hangeulSyllables').textContent = '';
+}
+
+// more actions --------------------------------------------------------------------------------
+
+function toggleSyllables(){
+  console.log('toggleSyllables');
+  let container = get('hangeulSyllables');
+  container.textContent = '';
+  getNumberAsHangeul().split('').forEach(syllable => {
+    let child = document.createElement('button');
+    child.innerText = syllable;
+    child.onclick = () => say(syllable);
+    container.appendChild(child);
+  })
 }
