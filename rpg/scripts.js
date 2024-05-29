627439b89c5378950fc4b1cbb276e3702bfb6874
Martín Farias <martin.akg1991@gmail.com>
2024-05-29 18:15:22 -0300

    RPG - version 1

---
+++ rpg/scripts.js
@@ -0,0 +1,10 @@
+function startGame() {
+  const name = document.getElementById('name');
+  const color = document.getElementById('color');
+
+  console.log('game started!');
+  console.log('name:', name.value);
+  console.log('color:', color.value);
+
+
+}
