ea32d5d8051c31f6ad8116d5049c617266444647
Martín Farias <martin.akg1991@gmail.com>
2024-05-29 18:44:37 -0300

    RPG - hides menu

--- rpg/scripts.js
+++ rpg/scripts.js
@@ -1,10 +1,37 @@
+const resultado = check();
+if (resultado) {
+  const gameEl = document.getElementById('game');
+
+  gameEl.classList.remove('hidden');
+  
+  const menuEl = document.getElementById('menu');
+
+  menuEl.classList.add('hidden');
+}
+
+
+
 function startGame() {
-  const name = document.getElementById('name');
-  const color = document.getElementById('color');
+  const nameElement = document.getElementById('name');
+  const colorElement = document.getElementById('color');
 
-  console.log('game started!');
-  console.log('name:', name.value);
-  console.log('color:', color.value);
+  // console.log('game started!');
+  // console.log('name:', nameElement.value);
+  // console.log('color:', color.value);
 
+  const urlParams = new URLSearchParams(location.search);
 
+  urlParams.set('name', nameElement.value);
+  urlParams.set('color', colorElement.value);
+  
+  location.search = urlParams;
+}
+
+function check() {
+  const urlParams = new URLSearchParams(location.search);
+
+  const name = urlParams.get('name');
+  const color = urlParams.get('color');
+
+  return !!name && !!color;
 }
