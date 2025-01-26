b4db873b9936312a1b45a3bee6904dae9e76757b
Martín Farias <martin.akg1991@gmail.com>
2025-01-26 20:08:43 -0300

    japanese: template for data format

--- japanese/data/phrases.js
+++ japanese/data/phrases.js
@@ -1,11 +1,66 @@
 export const data = [
-  ["こちらは私の彼女です。", "こちらははたしのかのしょです。", "This is my girlfriend."],
-  ["彼女はおしゃれな人です。", "かのしょはおしゃれなひとです。", "My girlfriend is a stylish person."],
-  ["私の彼女は元気な人です。 可愛いものが好きです。", "わたしのかのじょはげんきなひとです。かわいいものがすきです。", "My girlfriend is a lively person. She likes cute things."],
-  ["この電車は渋谷駅に行きますか ", "このでんしゃはしぶやえきにいきますか ", "Does this train go to Shibuya Station?"],
-  ["私の趣味はゲームと読みです。", "わたしのしゅみはゲームとよみです。", "My hobbies are playing games and reading."],
-  ["ビールを飲みますか  いいえ、ビールは 飲みません。", "ビールをのみますか  いいえ、 ビールをのみません。", "Do you drink beer？ No, I don’t drink beer."],
-  ["毎週木曜日に弟と晩御飯を作ります。 今週何を作りましょうか", "まいしゅうもくようびにおとうととばんごはんをつくります。こんしゅうなにをつくりましょうか。 ", "I make dinner with my (younger) brother every Thursday. What should I make this week?"],
-  ["日本のゲームも翻訳します。", "にほんのゲームもほにゃくします。", "I also translate Japanese games."],
-  ["今日はとても寒いです。 明日も寒いです。 ", "きょうはとてもさむいです。あしたもさむいです。", "Today is very cold. Tomorrow will also be cold."],
+  [
+    "こちらは私の彼女です。",
+    "こちらははたしのかのしょです。",
+    "This is my girlfriend.",
+  ], [
+    "彼女はおしゃれな人です。",
+    "かのしょはおしゃれなひとです。",
+    "My girlfriend is a stylish person.",
+  ], [
+    "私の彼女は元気な人です。 可愛いものが好きです。",
+    "わたしのかのじょはげんきなひとです。かわいいものがすきです。",
+    "My girlfriend is a lively person. She likes cute things.",
+  ],
+  [
+    "この電車は渋谷駅に行きますか ",
+    "このでんしゃはしぶやえきにいきますか ",
+    "Does this train go to Shibuya Station?",
+  ], [
+    "私の趣味はゲームと読みです。",
+    "わたしのしゅみはゲームとよみです。",
+    "My hobbies are playing games and reading.",
+  ],
+  [
+    "ビールを飲みますか  いいえ、ビールは 飲みません。",
+    "ビールをのみますか  いいえ、 ビールをのみません。",
+    "Do you drink beer？ No, I don’t drink beer.",
+  ], [
+    "毎週木曜日に弟と晩御飯を作ります。 今週何を作りましょうか",
+    "まいしゅうもくようびにおとうととばんごはんをつくります。こんしゅうなにをつくりましょうか。 ",
+    "I make dinner with my (younger) brother every Thursday. What should I make this week?",
+  ], [
+    "日本のゲームも翻訳します。",
+    "にほんのゲームもほにゃくします。",
+    "I also translate Japanese games.",
+  ], [
+    "今日はとても寒いです。 明日も寒いです。 ",
+    "きょうはとてもさむいです。あしたもさむいです。",
+    "Today is very cold. Tomorrow will also be cold.",
+  ],
+  [
+    "日本語の授業が全然わかりません。困ります。",
+    "にほんごのじゅぎょうがぜんぜんわかりません。こまります。",
+    "I do not understand Japanse class at all. I am in trouble.",
+  ], [
+    "いつか日本に行きます。",
+    "いつかにほんにいきます。",
+    "I will go to Japan one day.",
+  ], [
+    "日本語は独学です。",
+    "にほんごはどくがくです。",
+    "My Japanses is self-taught.",
+  ], [
+    "面白いから日本語を勉強します。",
+    "おもしろいからにほんごをべんきょうします。",
+    "Because it is interesting, I study Japanese.",
+  ], [
+    "予報によると、明日は雨だそうだ。",
+    "よほうによると、あしたはあめだそうだ。",
+    "According to the forecast, it will rain tomorrow.",
+  ], [
+    "後でやります。",
+    "あとでやります。",
+    "I will do it later.",
+  ],
 ];
