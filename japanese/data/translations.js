447f6dc3f78dca7ec0025272e0b0ed7ec794d718
Martín Farias <martin.akg1991@gmail.com>
2025-06-25 03:49:12 -0300

    japanese: refactor. Adds content page base.

--- japanese/data/translations.js
+++ japanese/data/translations.js
@@ -8,7 +8,7 @@ export const translations = [
       subtitle: 'Novela: Makoto Fukami, Original: Kentaro Miura',
     },
     'eng': {
-      title: '[Novel] Berserk: The Knight of the Fire Dragon',
+      title: '[Novel] Berserk: The Flame Dragon Knight',
       subtitle: 'Novel: Makoto Fukami, Original work: Kentaro Miura',
     }
   },
@@ -29,6 +29,33 @@ export const translations = [
       '実在の人物・団体・事件などにはいっさい関係ありません。',
       'YOUNG ANIMAL COMICS',
     ],
-    showPage: true,
+    showPageNumber: true,
+  },
+  {
+    content: [
+      'それはきっと美しいおとぎ話になるだろう。',
+      '一人の勇敢な乙女が身を捧げて、残忍な竜の暴虐を上めたというおとぎ話。',
+      'だが、それは真実ではない。美しい真実はこの世界の例外にすぎない。',
+      'いまから始まるのは、おとぎ話になる前の、この世界ではありふれた戦いの記録。',
+      '',
+      'ここは剣と暴力が支配する大陸。唯一神への信仰を絶対とする法王庁の異端審問が暴威を振るい、大国が覇を競い戦乱の時代が続く世界。',
+    ],
+    'esp': [
+      'Sin duda, será un hermoso cuento de hadas.',
+      'Una valiente doncella que se sacrificó para poner fin a la tiranía de un cruel dragón.',
+      'Pero no es la verdad. La hermosa verdad es solo una excepción en este mundo.',
+      'Lo que comienza ahora es el relato de una lucha común en este mundo, antes de convertirse en un cuento de hadas.',
+      '',
+      'Aquí, en un continente dominado por espadas y violencia, donde la Inquisición del Santo Oficio, que considera la fe en el Dios Único como absoluta, ejerce su tiranía, y las grandes naciones compiten por la supremacía en una era de guerra continua.',
+    ],
+    'eng': [
+      'It will surely be a beautiful fairy tale.',
+      'A brave maiden who sacrificed herself to end the tyranny of a cruel dragon.',
+      'But that is not the truth. The beautiful truth is merely an exception in this world.',
+      'What begins now is the record of a common struggle in this world, before it becomes a fairy tale.',
+      '',
+      'Here, on a continent ruled by swords and violence, where the Inquisition of the Holy See, which considers faith in the One God as absolute, exerts its tyranny, and great nations compete for supremacy in an era of continuous warfare.',
+    ],
+    showPageNumber: true,
   }
 ];
