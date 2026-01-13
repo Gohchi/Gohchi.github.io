aafef87e50dfc0d5afc200072292e0ebb68dc869
Martín Farias <martin.akg1991@gmail.com>
2026-01-13 20:24:14 -0300

    japanese: NEW! PWA mode

--- japanese/sw.js
+++ japanese/sw.js
@@ -1,20 +1,15 @@
-async function addResourcesToCache(storeName, resources) {
-  const cache = await caches.open(storeName);
-  await cache.addAll(resources);
-};
-
 self.addEventListener('install', e => {
   e.waitUntil(
-    addResourcesToCache(
-      'japanese-store',
-      [
-        '/japanese/',
-        '/japanese/index.html',
-        '/japanese/scripts.js',
-        '/japanese/styles.css'
-      ]
-    )
-  );
+    caches.open('japanese-store')
+      .then(function(cache) {
+        return cache.addAll([
+          '/japanese/',
+          '/japanese/index.html',
+          '/japanese/scripts.js',
+          '/japanese/styles.css'
+        ]);
+      })
+  )
 });
 
 self.addEventListener('fetch', e => {
